22862d6ec3cc50c351a78259be220144f40598c7
Souvik-R <souvikr212@gmail.com>
2026-02-17 03:00:52 +0530

    change

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config();
 
 
-
+connectDB();
 
 
 
