f0ee9038a8b7ddfd843fa65f06103363f6de554c
Souvik-R <souvikr212@gmail.com>
2026-02-23 23:36:07 +0530

    modified

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config();
 
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is listening on port: ${process.env.PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 
 
 /* import express from "express";
